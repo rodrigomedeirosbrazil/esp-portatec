849dd2624c0745b832dd5b7616868724ab703834
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-05-15 08:27:05 -0300

    chore: add captive portal

--- src/main.cpp
+++ src/main.cpp
@@ -12,29 +12,50 @@
 
 const char *ssid = APSSID;
 const char *password = APPSK;
+IPAddress myIP;
 
+DNSServer dnsServer;
 ESP8266WebServer server(80);
 
+void handleNotFound();
+void handleNotFound() {
+  server.sendHeader("Location", "http://" + myIP.toString(), true);
+  server.send(302, "text/plain", "");
+}
+
+void handleRoot();
 void handleRoot() {
-  server.send(200, "text/html", "<h1>You are connected</h1>");
+  String html = "<!DOCTYPE html><html><head>";
+  html += "<meta http-equiv='refresh' content='0;url=http://" + myIP.toString() + "'>";
+  html += "<title>Portal de Autenticação</title></head>";
+  html += "<body><h1>Bem-vindo ao ESP-PORTATEC</h1>";
+  html += "<p>Por favor, autentique-se para acessar a internet</p>";
+  html += "<form method='POST' action='/login'>";
+  html += "<input type='text' name='username' placeholder='Usuário'><br>";
+  html += "<input type='password' name='password' placeholder='Senha'><br>";
+  html += "<button type='submit'>Conectar</button></form></body></html>";
+  server.send(200, "text/html", html);
 }
 
 void setup() {
   delay(1000);
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println();
   Serial.print("Configuring access point...");
 
   WiFi.softAP(ssid, password);
 
-  IPAddress myIP = WiFi.softAPIP();
+  myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
   server.on("/", handleRoot);
+  server.onNotFound(handleNotFound);
   server.begin();
+  dnsServer.start(53, "*", myIP);
   Serial.println("HTTP server started");
 }
 
 void loop() {
   server.handleClient();
+  dnsServer.processNextRequest();
 }
